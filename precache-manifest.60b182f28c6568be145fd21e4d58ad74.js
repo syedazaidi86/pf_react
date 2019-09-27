50652a913b789a12bd67e3094e4c10b0e06f8342
Syeda Midhat Zaidi <midhatzaidi@hotmail.com>
2019-09-28 01:38:47 +1000

    Updates

---
+++ precache-manifest.60b182f28c6568be145fd21e4d58ad74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa39f308636bbbd01c142e89ba2c2217",
+    "url": "/pf_react/index.html"
+  },
+  {
+    "revision": "cef3c5974988adae37b2",
+    "url": "/pf_react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0ff5463b219abd856761",
+    "url": "/pf_react/static/js/2.15e16aa8.chunk.js"
+  },
+  {
+    "revision": "cef3c5974988adae37b2",
+    "url": "/pf_react/static/js/main.88dfa7e0.chunk.js"
+  },
+  {
+    "revision": "1fbc0129955eaee7780d",
+    "url": "/pf_react/static/js/runtime-main.d2e2729c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/pf_react/static/media/logo.25bf045c.svg"
+  }
+]);
